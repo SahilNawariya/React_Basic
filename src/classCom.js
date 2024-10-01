6544bd276f33761251452aa3923e86c52e258f61
SahilNawariya <snawariya75@gmail.com>
2024-10-01 17:11:35 +0530

    commit message from terminal

--- src/classCom.js
+++ src/classCom.js
@@ -1,34 +1,45 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Users from "./funCom";
 import './App.css';
-export default class User2 extends Component{
-    constructor(){
+export default class User2 extends Component {
+    constructor() {
         super();
-        this.state={
-            data:"Sahil"
+        console.log("Constructur");
+
+        this.state = {
+            data: "Sahil"
         }
     }
- /*    apple(){
-        this.setState({
-            data:'Sahil Nawariya'
-        })
-    } */
+    /*    apple(){
+           this.setState({
+               data:'Sahil Nawariya'
+           })
+       } */
+    componentDidMount() {
+        console.log("componentDidMount");
 
-   
-    render(){
-        console.log(this.props);
-        return(
+    }
+
+    componentDidUpdate(){
+        console.log("componentDidUpdate");
+    }
+    render() {
+        console.log("Render");
+
+        return (
             <div>
-               {/*  <div className="App">
+                {/*  <div className="App">
                     <h1>{this.state.data}</h1>
                     <button onClick={()=>this.setState({data:'Sahil Nawariya'})}>Update Data</button>
-                </div> */}
+                </div> 
                 <div className="App">
                     <h1>{this.props.name[1]}</h1>
                     <h1>{this.props.email[1]}</h1>
                     <h1>{this.props.other.Address}</h1>
                     <h1>{this.props.other.State}</h1>
-                </div>
+                </div>*/}
+                <h1>{this.state.data}</h1>
+                <button onClick={()=>this.setState({data:'Sahil Nawariya'})}>COme</button>
             </div>
         )
     }
